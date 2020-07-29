d5cdbc84b3f1563e9d365277636da82e82382504
Bare Necessities <oluwatosinJAjao@outlook.com>
2020-07-29 05:42:05 -0700

    Imported all old code

--- app/src/main/java/com/example/materialdesign/MainActivity.java
+++ app/src/main/java/com/example/materialdesign/MainActivity.java
@@ -1,14 +1,38 @@
+
 package com.example.materialdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.materialdesign.Login;
+import com.example.materialdesign.R;
 
 public class MainActivity extends AppCompatActivity {
+    Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myButton = findViewById(R.id.SignUp);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent move = new Intent(MainActivity.this, com.example.materialdesign.SignUp.class);
+                startActivity(move);
+            }
+        });
+        myButton = findViewById(R.id.LogIn);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent move = new Intent(MainActivity.this, Login.class);
+                startActivity(move);
+            }
+        });
     }
 }
