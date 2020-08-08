9eb4af3b6bf8c1bcc032738339e70fb25a439ed2
Bare Necessities <oluwatosinJAjao@outlook.com>
2020-08-08 00:46:13 -0700

    Cassava farms

--- app/src/main/java/com/example/materialdesign/CassavaFarms.java
+++ app/src/main/java/com/example/materialdesign/CassavaFarms.java
@@ -1,9 +1,9 @@
 package com.example.materialdesign;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.os.Bundle;
 
 public class CassavaFarms extends AppCompatActivity {
 
@@ -14,5 +14,21 @@ public class CassavaFarms extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(getString(R.string.app_name));
         toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+
+        Toolbar barChart = findViewById(R.id.barChart);
+        barChart.setTitle("35% ROI in 7 months");
+        barChart.setNavigationIcon(R.drawable.ic_baseline_multiline_chart_24);
+
+        Toolbar wallet = findViewById(R.id.wallet);
+        wallet.setTitle("#50,000");
+        wallet.setNavigationIcon(R.drawable.ic_baseline_account_balance_wallet);
+
+        Toolbar locator = findViewById(R.id.location);
+        locator.setTitle("Oyo");
+       locator.setNavigationIcon(R.drawable.ic_baseline_location_on);
+
+
+
+
     }
 }
